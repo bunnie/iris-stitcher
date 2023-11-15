e7413ea0d004e41e222c9f964073c4caca34f89d
bunnie <bunnie@kosagi.com>
2023-11-16 01:53:27 +0800

    add a script to adjust names based on a scale
    this is to compensate for offsets in the mechanics itself
    post-imaging. This should probably actually be calibrated
    out before generating a data set, but it is what it is.

---
+++ adjust-names.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+import argparse
+from pathlib import Path
+from schema import Schema
+import numpy as np
+import shutil
+
+def main():
+    parser = argparse.ArgumentParser(description="IRIS Stitching Scripts")
+    parser.add_argument(
+        "--src", required=True, help="source dir"
+    )
+    parser.add_argument(
+        "--dst", required=True, help="destination dir"
+    )
+    parser.add_argument(
+        "--dry-run", help="Do a dry run (don't copy any files)", default=False, action='store_true'
+    )
+    parser.add_argument(
+        "--x", required=False, help="Rescale factor for x positions", default=1.0, type=float
+    )
+    parser.add_argument(
+        "--y", required=False, help="Rescale factor for y positions", default=1.0, type=float
+    )
+    args = parser.parse_args()
+
+    src_path = Path("raw/" + args.src)
+    src_files = [file for file in src_path.glob('*.png') if file.is_file()]
+
+    dst_path = Path("raw/" + args.dst)
+
+    coords_mm = []
+    for file in src_files:
+        metadata = Schema.meta_from_fname(file.stem)
+        coords_mm += [(metadata['x'], metadata['y'])]
+
+    coords = np.unique(coords_mm, axis=0)
+    dists = []
+    for p in coords:
+        dists += [np.linalg.norm(p - [-100, -100])]
+    ll_centroid = coords[dists.index(min(dists))]
+
+    # ll_centroid is our "reference point". From here, we scale everything else.
+    for file in src_files:
+        metadata = Schema.meta_from_fname(file.stem)
+        metadata['x'] = (float(metadata['x']) - ll_centroid[0]) * args.x + ll_centroid[0]
+        metadata['y'] = (float(metadata['y']) - ll_centroid[1]) * args.y + ll_centroid[1]
+        new_fname = Schema.fname_from_meta(metadata)
+        if args.dry_run:
+            print(f"{file.stem} -> {new_fname}")
+        else:
+            dest_path = Path(f"raw/{args.dst}") / (new_fname + ".png")
+            print(f"copying {file} -> {dest_path}")
+            shutil.copy(file, dest_path)
+
+if __name__ == "__main__":
+    main()
