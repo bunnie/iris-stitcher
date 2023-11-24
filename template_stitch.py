7ff1c42573bf5decd017ffb3c3d81147240c1df4
bunnie <bunnie@kosagi.com>
2023-11-24 23:28:49 +0800

    take at a second algorithm for automated template stitching
    this time we look only for regions of maximum overlap to stitch
    various images together to improve our chances of finding a
    unique, high-quality solution.

--- template_stitch.py
+++ template_stitch.py
@@ -5,7 +5,10 @@ import numpy as np
 import logging
 
 # Use template matching of laplacians to do stitching
-def stitch_one_template(self, ref_img, ref_meta, ref_t, moving_img, moving_meta, moving_t, moving_layer):
+def stitch_one_template(self,
+                        ref_img, ref_meta, ref_t,
+                        moving_img, moving_meta, moving_t,
+                        moving_layer):
     nominal_vector_px = (
         (moving_meta['x'] - ref_meta['x'] + moving_t['offset'][0]) * 1000 * Schema.PIX_PER_UM,
         (moving_meta['y'] - ref_meta['y'] + moving_t['offset'][1]) * 1000 * Schema.PIX_PER_UM
@@ -26,7 +29,7 @@ def stitch_one_template(self, ref_img, ref_meta, ref_t, moving_img, moving_meta,
     template = moving_img[
         int(intersected_rect.tl.y) : int(intersected_rect.br.y),
         int(intersected_rect.tl.x) : int(intersected_rect.br.x)
-    ]
+    ].copy()
     w, h = template.shape[::-1]
     # All the 6 methods for comparison in a list
     #methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
@@ -107,7 +110,6 @@ def stitch_one_template(self, ref_img, ref_meta, ref_t, moving_img, moving_meta,
     check_t = self.schema.schema['tiles'][str(moving_layer)]
     logging.info(f"after adjustment: {check_t['offset'][0]},{check_t['offset'][1]}")
 
-
 def stitch_auto_template(self):
     STRIDE_X_MM = 0.1
     STRIDE_Y_MM = 0.1
@@ -116,33 +118,117 @@ def stitch_auto_template(self):
     (ref_layer, ref_t) = self.schema.get_tile_by_coordinate(self.schema.tl_centroid)
     assert ref_layer is not None, "Couldn't find initial tile!"
     ref_img = self.schema.get_image_from_tile(ref_t)
+    ref_t['score'] = 1.0 # made up number that is positive to indicate we've been placed
+    ref_t['auto_error'] = 'false'
 
     extents = self.schema.br_centroid
 
-    for y in np.arange(self.schema.tl_centroid[1], extents[1], STRIDE_Y_MM):
-        if ref_layer is not None:
-            for x in np.arange(self.schema.tl_centroid[0], extents[0], STRIDE_X_MM):
-                logging.info(f"{x}, {y}")
-                (moving_layer, moving_t) = self.schema.get_tile_by_coordinate(Point(x, y))
-                if moving_layer == ref_layer or moving_layer is None:
+    if False:
+        for y in np.arange(self.schema.tl_centroid[1], extents[1], STRIDE_Y_MM):
+            if ref_layer is not None:
+                for x in np.arange(self.schema.tl_centroid[0], extents[0], STRIDE_X_MM):
+                    logging.info(f"{x}, {y}")
+                    (moving_layer, moving_t) = self.schema.get_tile_by_coordinate(Point(x, y))
+                    if moving_layer == ref_layer or moving_layer is None:
+                        continue
+                    else:
+                        logging.info(f"Trying to stitch {ref_layer} and {moving_layer}")
+                        moving_img = self.schema.get_image_from_tile(moving_t)
+                        self.stitch_one_template(
+                            ref_img, Schema.meta_from_tile(ref_t), ref_t,
+                            moving_img, Schema.meta_from_tile(moving_t), moving_t, moving_layer
+                        )
+                        ref_layer = moving_layer
+                last_ref = ref_layer
+                ref_layer = None
+            else:
+                (candidate_layer, candidate_t) = self.schema.get_tile_by_coordinate(Point(x, y))
+                if candidate_layer == last_ref or candidate_layer is None:
                     continue
                 else:
-                    logging.info(f"Trying to stitch {ref_layer} and {moving_layer}")
+                    ref_layer = candidate_layer
+                    ref_t = candidate_t
+                    ref_img = self.schema.get_image_from_tile(ref_t)
+    else:
+        did_run = False
+        for y in np.arange(self.schema.tl_centroid[1], extents[1], STRIDE_Y_MM):
+            for x in np.arange(self.schema.tl_centroid[0], extents[0], STRIDE_X_MM):
+                logging.info(f"{x}, {y}")
+                overlaps = self.schema.get_intersecting_tiles(Point(x, y))
+                pairs = []
+                for (layer, t) in overlaps:
+                    for (layer_o, t_o) in overlaps:
+                        if layer != layer_o:
+                            layer_meta = Schema.meta_from_tile(t)
+                            r1 = Schema.rect_mm_from_center(Point(layer_meta['x'], layer_meta['y']))
+                            layer_o_meta = Schema.meta_from_tile(t_o)
+                            r2 = Schema.rect_mm_from_center(Point(layer_o_meta['x'], layer_o_meta['y']))
+                            pair = RectPair(r1, layer, t, r2, layer_o, t_o)
+                            if pair.is_candidate():
+                                unique = True
+                                for p in pairs:
+                                    if pair.is_dupe(p):
+                                        unique = False
+                                        break
+                                if unique:
+                                    pairs += [
+                                        RectPair(r1, layer, t, r2, layer_o, t_o)
+                                    ]
+
+                if len(pairs) > 0:
+                    candidates = sorted(pairs, reverse = True)
+                    (ref_r, ref_layer, ref_t) = candidates[0].get_ref()
+                    ref_img = self.schema.get_image_from_tile(ref_t)
+                    (moving_r, moving_layer, moving_t) = candidates[0].get_moving()
                     moving_img = self.schema.get_image_from_tile(moving_t)
+                    logging.info(f"Stitching {ref_layer}:{ref_r} to {moving_layer}:{moving_r}")
                     self.stitch_one_template(
                         ref_img, Schema.meta_from_tile(ref_t), ref_t,
                         moving_img, Schema.meta_from_tile(moving_t), moving_t, moving_layer
                     )
-                    ref_layer = moving_layer
-            last_ref = ref_layer
-            ref_layer = None
+                    did_run = True
+            if did_run:
+                break
+
+class RectPair():
+    def __init__(self, r1: Rect, r1_layer, r1_t, r2: Rect, r2_layer, r2_t):
+        self.r1 = r1
+        self.r2 = r2
+        self.r1_layer = r1_layer
+        self.r2_layer = r2_layer
+        self.r1_t = r1_t
+        self.r2_t = r2_t
+        self.overlap = self.r1.intersection(self.r2)
+        if self.overlap is not None:
+            self.overlap_area = self.overlap.area()
         else:
-            (candidate_layer, candidate_t) = self.schema.get_tile_by_coordinate(Point(x, y))
-            if candidate_layer == last_ref or candidate_layer is None:
-                continue
-            else:
-                ref_layer = candidate_layer
-                ref_t = candidate_t
-                ref_img = self.schema.get_image_from_tile(ref_t)
+            self.overlap_area = 0.0
 
+    def __lt__(self, other):
+        return self.overlap_area < other.overlap_area
 
+    def __eq__(self, other):
+        return round(self.overlap_area, 5) == round(other.overlap_area, 5)
+
+    def is_dupe(self, rp):
+        return (rp.r1 == self.r1 and rp.r2 == self.r2) or (rp.r1 == self.r2 and rp.r2 == self.r1)
+
+    # True if exactly one of the pair has been scored, and the other has not (basically an XOR function).
+    def is_candidate(self):
+        return (self.r1_t['score'] < 0.0 and self.r2_t['score'] > 0.0) or (self.r1_t['score'] > 0.0 and self.r2_t['score'] < 0.0)
+
+    def get_ref(self):
+        if self.r1_t['score'] < 0.0 and self.r2_t['score'] > 0.0:
+            return (self.r2, self.r2_layer, self.r2_t)
+        elif self.r2_t['score'] < 0.0 and self.r1_t['score'] > 0.0:
+            return (self.r1, self.r1_layer, self.r1_t)
+        else:
+            return None
+
+    def get_moving(self):
+        if self.r1_t['score'] < 0.0 and self.r2_t['score'] > 0.0:
+            return (self.r1, self.r1_layer, self.r1_t)
+        elif self.r2_t['score'] < 0.0 and self.r1_t['score'] > 0.0:
+            return (self.r2, self.r2_layer, self.r2_t)
+        else:
+            return None
