199d5cffd4602a47ff6be96138204a522835f014
bunnie <bunnie@kosagi.com>
2023-11-27 17:21:49 +0800

    start from anchor and "roll" stitching from there
    also various intermediate debug patches

--- template_stitch.py
+++ template_stitch.py
@@ -59,6 +59,7 @@ def stitch_one_template(self,
         int(intersected_rect.tl.y) : int(intersected_rect.br.y),
         int(intersected_rect.tl.x) : int(intersected_rect.br.x)
     ].copy()
+    # stash a copy of the "before" stitch image
     ref_initial = intersected_rect.translate(nominal_vector_px)
     before = np.hstack((
         cv2.resize(template, None, None, 0.3, 0.3),
@@ -85,22 +86,16 @@ def stitch_one_template(self,
         res = cv2.matchTemplate(ref_laplacian, template_laplacian, METHOD)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         top_left = max_loc
+        res_8u = cv2.normalize(res, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        ret, thresh = cv2.threshold(res_8u, 224, 255, 0)
     else:
         METHOD = cv2.TM_SQDIFF  # squared error matching
         res = cv2.matchTemplate(ref_laplacian, template_laplacian, METHOD)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         top_left = min_loc
-    # bottom_right = (top_left[0] + w, top_left[1] + h)
-    # cv2.rectangle(ref_img, top_left, bottom_right, 255, 2)
-    SCALE = 0.5
-    # cv2.imshow('template', cv2.resize(template, None, None, SCALE, SCALE))
-    # cv2.imshow('matching result', cv2.resize(cv2.normalize(res, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX), None, None, SCALE, SCALE))
-    # cv2.imshow('matching result', cv2.normalize(res, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX))
-    res_8u = cv2.normalize(res, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    #cv2.imshow('matching result', res_8u)
-    ret, thresh = cv2.threshold(res_8u, 192, 255, 0)
-    #cv2.imshow('thresh', thresh)
-    #cv2.waitKey()
+        res_8u = cv2.normalize(res, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        res_8u = 255 - res_8u # invert the thresholding
+        ret, thresh = cv2.threshold(res_8u, 224, 255, 0)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(res_8u, contours, -1, (0,255,0), 1)
@@ -123,10 +118,6 @@ def stitch_one_template(self,
                 logging.info(f"{top_left} is contained within a donut-shaped region. Suspect blurring error!")
                 has_single_solution = False
 
-    # cv2.imshow('detected point', cv2.resize(ref_img, None, None, SCALE, SCALE))
-    # cv2.imshow('moving image', cv2.resize(moving_img, None, None, SCALE, SCALE))
-    # cv2.waitKey()
-
     adjustment_vector_px = Point(
         -nominal_vector_px[0] - (intersected_rect.tl.x - top_left[0]),
         -nominal_vector_px[1] - (intersected_rect.tl.y - top_left[1])
@@ -143,14 +134,15 @@ def stitch_one_template(self,
     self.schema.store_auto_align_result(
         moving_layer,
         score,
-        has_single_solution,
+        not has_single_solution,
     )
     check_t = self.schema.schema['tiles'][str(moving_layer)]
     logging.info(f"after adjustment: {check_t['offset'][0]:0.2f},{check_t['offset'][1]:0.2f}")
 
+    # assemble the before/after images
     after_vector_px = Point(
-        ((moving_meta['x'] - ref_meta['x']) * 1000 + moving_t['offset'][0]) * Schema.PIX_PER_UM,
-        ((moving_meta['y'] - ref_meta['y']) * 1000 + moving_t['offset'][1]) * Schema.PIX_PER_UM
+        ((moving_meta['x'] - ref_meta['x']) * 1000 + check_t['offset'][0]) * Schema.PIX_PER_UM,
+        ((moving_meta['y'] - ref_meta['y']) * 1000 + check_t['offset'][1]) * Schema.PIX_PER_UM
     )
     ref_after = intersected_rect.translate(after_vector_px)
     after = np.hstack(pad_images_to_same_size(
@@ -162,9 +154,15 @@ def stitch_one_template(self,
             ], None, None, 0.3, 0.3)
         )
     ))
+    overview = np.hstack(pad_images_to_same_size(
+        (
+            cv2.resize(moving_img, None, None, 0.15, 0.15),
+            cv2.resize(ref_img, None, None, 0.15, 0.15)
+        )
+    ))
     before_after = np.vstack(
         pad_images_to_same_size(
-            (before, after)
+            (before, after, overview)
         )
     )
     cv2.imshow('before/after', before_after)
@@ -175,20 +173,41 @@ def stitch_auto_template(self):
     STRIDE_X_MM = Schema.NOM_STEP
     STRIDE_Y_MM = Schema.NOM_STEP
 
-    # start from the smallest coordinates in x/y and work our way up along X, then along Y.
-    (ref_layer, ref_t) = self.schema.get_tile_by_coordinate(self.schema.tl_centroid)
-    assert ref_layer is not None, "Couldn't find initial tile!"
-    ref_img = self.schema.get_image_from_tile(ref_t)
-    ref_t['score'] = 1.0 # made up number that is positive to indicate we've been placed
-    ref_t['auto_error'] = 'false'
-
     extents = self.schema.br_centroid
+    # find an anchor layer
+    # start from the smallest coordinates in x/y and work our way up along X, then along Y.
+    found_anchor = False
+    x_roll = 0
+    y_roll = 0
+    y_indices = np.arange(self.schema.tl_centroid[1], extents[1], STRIDE_Y_MM)
+    x_indices = np.arange(self.schema.tl_centroid[0], extents[0], STRIDE_X_MM)
+    y_steps = 0
+    for y in y_indices:
+        x_steps = 0
+        for x in x_indices:
+            (ref_layer, ref_t) = self.schema.get_tile_by_coordinate(Point(x, y))
+            if ref_t is not None:
+                if ref_t['auto_error'] == 'anchor':
+                    ref_img = self.schema.get_image_from_tile(ref_t)
+                    ref_t['score'] = 1.0 # made up number that is positive to indicate we've been placed
+                    ref_t['auto_error'] = 'anchor'
+                    found_anchor = True
+                    x_roll = x_steps
+                    y_roll = y_steps
+            x_steps += 1
+        y_steps += 1
+    if not found_anchor:
+        logging.error("No anchor layer set, can't proceed with stitching")
+        return
+    y_indices = np.roll(y_indices, -y_roll)
+    x_indices = np.roll(x_indices, -x_roll)
 
-    for y in np.arange(self.schema.tl_centroid[1], extents[1], STRIDE_Y_MM):
-        for x in np.arange(self.schema.tl_centroid[0], extents[0], STRIDE_X_MM):
+    # start stitching from the anchor point
+    for y in y_indices:
+        for x in x_indices:
             overlaps = self.schema.get_intersecting_tiles(Point(x, y))
             combs = list(combinations(overlaps, 2))
-            print(f"number of combination @ ({x:0.2f}, {y:0.2f}): {len(combs)}")
+            logging.info(f"number of combinations @ ({x:0.2f}, {y:0.2f}): {len(combs)}")
             pairs = []
             for ((layer, t), (layer_o, t_o)) in combs:
                 # filter by candidates only -- pairs that have one aligned, and one aligned image.
@@ -209,7 +228,7 @@ def stitch_auto_template(self):
                 ref_img = self.schema.get_image_from_tile(ref_t)
                 (moving_r, moving_layer, moving_t) = candidates[0].get_moving()
                 moving_img = self.schema.get_image_from_tile(moving_t)
-                logging.debug(f"Stitching {ref_layer}:{ref_r} to {moving_layer}:{moving_r}")
+                logging.info(f"Stitching {ref_layer}:{ref_r.center()} to {moving_layer}:{moving_r.center()}")
                 self.stitch_one_template(
                     ref_img, Schema.meta_from_tile(ref_t), ref_t,
                     moving_img, Schema.meta_from_tile(moving_t), moving_t, moving_layer
